43f2444f4e12fc5c0b1a8afea52d9efae930ea71
K0sarr <Kosar_Abobakri@yahoo.com>
2024-10-31 17:27:56 +0100

    Installatiation of Google-oauth
    I have uninstalled react-google-login from the project and have implemented React-oauth-google since it is better supported for react 18 and above. Also cleaned up some code in login with console logs, etc. Adjusted the login & logout with the new package.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-// import GoogleLogin from 'react-google-login'
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { GoogleLogin, googleLogout } from '@react-oauth/google'
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 import jwt_decode from 'jwt-decode';
 import { useNavigate } from 'react-router-dom'
 import shareVideo from '../assets/share.mp4'
@@ -12,22 +10,16 @@ import {client} from '../client'
 
 const Login = () => {
 
-    // const responseGoogle = (response) => {
-    //     console.log(response);
-    //   }
+
     const navigate = useNavigate();
 
     const responseGoogle = (response) => {
       const decode = jwt_decode(response.credential);
-      console.log(decode);
-        // if (!response.profileObj) return;
-        // localStorage.setItem('user', JSON.stringify(response.profileObj))
+      // console.log(decode);
 
         localStorage.setItem('user', JSON.stringify(decode));
 
-        // const { name, googleId, imageUrl } = response.profileObj;
         const { name, aud, picture } = decode;
-        // const { name, sub, imageUrl } = decode;
 
         const doc = {
           _id: aud, 
@@ -35,18 +27,6 @@ const Login = () => {
           userName: name,
           image: picture,
         }
-
-        // const doc = {
-        //     _id: sub,
-        //     _type: 'user',
-        //     userName: name,
-        //     image: imageUrl,
-        // }
-
-        console.log("googleId:", aud);
-        console.log("image:", picture);
-
-
 
         client.createIfNotExists(doc)
         .then(() => {
@@ -68,45 +48,19 @@ const Login = () => {
         />
 
         <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
-        <div className="p-5">
-        <img src={logo} width="130px" alt="logo" />
-        </div>
+          <div className="p-5">
+            <img src={logo} width="130px" alt="logo" />
+          </div>
 
-        <div className='shadow-2xl'>
-        <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
-          <GoogleLogin
-            onSuccess={responseGoogle}
-            onFailure={responseGoogle}
-            cookiePolicy="single_host_origin"
-          />
-        </GoogleOAuthProvider>
-
-
-            {/* <GoogleOAuthProvider
-             clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
+          <div className='shadow-2xl'>
+            <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
               <GoogleLogin
-             render={(renderProps) => (
-                <button
-                type="button"
-                className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
-                onClick={renderProps.onClick}
-                disabled={renderProps.disabled}
-                >
-                    <FcGoogle className="mr-4"/> Sign in with Google
-
-                </button>
-
-             )}
-            onSuccess={responseGoogle}
-            onFailure={responseGoogle}
-            cookiePolicy="single_host_origin"
-            
-            />;
-            </GoogleOAuthProvider>; */}
-            
-
-
-        </div>
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy="single_host_origin"
+              />
+            </GoogleOAuthProvider>
+          </div>
 
         </div>
       </div>
